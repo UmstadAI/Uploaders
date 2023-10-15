b4ad539c1c370866cb878d49314a87e92759db5e
dogukan <dogukanakarc@gmail.com>
2023-10-15 18:15:00 +0300

    add absolute path

--- doc_loader/utils/index.js
+++ doc_loader/utils/index.js
@@ -1,31 +1,32 @@
-import { mdxToMd } from "mdx-to-md"
-import fs from "fs"
+import { mdxToMd } from "mdx-to-md";
+import fs from "fs";
 import path from "path";
 
-const basePath = "../files"
+const absolutePath = path.resolve("../files");
+const basePath = "../files";
 
 const markdownFiles = [];
 
 function findMarkdownFiles(directory) {
-    fs.readdirSync(directory).forEach(file => {
-        const fullPath = path.join(directory, file);
-        if (fs.statSync(fullPath).isDirectory()) {
-            findMarkdownFiles(fullPath);
-        } else if (path.extname(fullPath).toLowerCase() === '.mdx') {
-            markdownFiles.push(fullPath);
-        }
-    });
+  fs.readdirSync(directory).forEach((file) => {
+    const fullPath = path.join(directory, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      findMarkdownFiles(fullPath);
+    } else if (path.extname(fullPath).toLowerCase() === ".mdx") {
+      markdownFiles.push(fullPath);
+    }
+  });
 }
 
-findMarkdownFiles(basePath);
-console.log(markdownFiles)
+findMarkdownFiles(absolutePath);
+console.log(markdownFiles);
 
 export async function mdxToMarkdown(markdownFiles) {
-    // TODO mdx to md
-    for (let i = 0; i < markdownFiles.length; i++) {
-        const markdown = await mdxToMd(markdownFiles[i])
-        console.log(markdown)
-    }
+  // TODO mdx to md
+  for (let i = 0; i < markdownFiles.length; i++) {
+    const markdown = await mdxToMd(markdownFiles[i]);
+    console.log(markdown);
+  }
 }
 
-mdxToMarkdown(markdownFiles)
+mdxToMarkdown(markdownFiles);
