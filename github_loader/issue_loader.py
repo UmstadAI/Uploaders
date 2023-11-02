abf3984666692562c6f7f14d908426da549a43c5
berkingurcan <berkingurcan@yandex.com>
2023-11-02 13:41:53 +0300

    use PyGithub to get rid of Rate Limit

--- github_loader/issue_loader.py
+++ github_loader/issue_loader.py
@@ -3,6 +3,7 @@ import requests
 import time
 import csv
 
+from github import Github
 from langchain.document_loaders import GitHubIssuesLoader
 
 token = os.getenv('GITHUB_ACCESS_TOKEN') or 'GITHUB_ACCESS_TOKEN'
@@ -22,25 +23,17 @@ def get_github_issue_and_comments(issue_link):
     parts = issue_link.strip('/').split('/')
     owner, repo, issue_number = parts[-4], parts[-3], parts[-1]
 
-    api_url = f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}"
+    g = Github(token)
+    repo = g.get_repo(f"{owner}/{repo}")
+    issue = repo.get_issue(int(issue_number))
 
-    response = requests.get(api_url)
-
-    if response.status_code == 200:
-        issue_data = response.json()
-        comments_url = issue_data['comments_url']
-        
-        comments_response = requests.get(comments_url)
-        
-        if comments_response.status_code == 200:
-            comments_data = comments_response.json()
-            return issue_data, comments_data
-        else:
-            print("Error fetching comments")
-            return None, f"Error fetching comments. Status code: {comments_response.status_code}"
-    else:
-        print("Error fetching issue")
-        return None, f"Error fetching issue. Status code: {response.status_code}"
+    try:
+        comments = issue.get_comments()
+        comments_data = [comment.raw_data for comment in comments]
+        return issue, comments
+    except Exception as e:
+        print(f"Error fetching comments: {e}")
+        return None, f"Error fetching comments: {e}"
 
 def extract_issue_data(issue_links):
     issues = []
@@ -50,17 +43,19 @@ def extract_issue_data(issue_links):
         if issue_data is None or comments_data is None:
             continue
 
-        issue_number = issue_data['number']
-        issue_title = issue_data['title']
-        issue_writer = issue_data['user']['login']
-        is_issue_open = issue_data['state'] == 'open'
-        issue_body = issue_data['body']
+        issue_number = issue_data.number
+        issue_title = issue_data.title
+        issue_writer = issue_data.user.login
+        is_issue_open = issue_data.state
+        issue_body = issue_data.body
 
         issue = {}
+
         comments = []
         for comment in comments_data:
-            comment_writer = comment['user']['login']
-            comment_body = comment['body']
+            print(comment)
+            comment_writer = comment.user.login
+            comment_body = comment.body
             # comment_reactions = comment['reactions']
             # comment_reactions.pop('url', None)
             comments.append((comment_writer, comment_body))
@@ -76,8 +71,6 @@ def extract_issue_data(issue_links):
         print(issues)
         x += 1
         
-        time.sleep(5)
-
     return issues
 
 issue_data = extract_issue_data(issue_links)
