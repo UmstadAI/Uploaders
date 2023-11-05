abe7279bf22fd4658e638947f88c80dbbd25bd4c
berkingurcan <berkingurcan@yandex.com>
2023-11-05 14:08:11 +0300

    Update loader.py

--- github_project_loader/tutorials_loader/loader.py
+++ github_project_loader/tutorials_loader/loader.py
@@ -1,7 +1,34 @@
+import glob
 import os
+import openai
+import pinecone
+import time
 
-path_example_docs = "../../doc_loader/files/docs2/zkapps/tutorials"
+from uuid import uuid4
+from langchain.document_loaders import UnstructuredMarkdownLoader
+from langchain.text_splitter import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter, Language
 
-files = os.listdir(path_example_docs)
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv(), override=True) # read local .env file
 
-md_files = [file for file in os.listdir(path_example_docs) if file.endswith(".md")]
+openai.api_key = os.getenv('OPENAI_API_KEY') or 'OPENAI_API_KEY'
+pinecone_api_key = os.getenv('PINECONE_API_KEY') or 'YOUR_API_KEY'
+pinecone_env = os.getenv('PINECONE_ENVIRONMENT') or "YOUR_ENV"
+
+path_tutorial_docs = "../../doc_loader/files/docs2/zkapps/tutorials"
+
+files = os.listdir(path_tutorial_docs)
+
+md_files = [file for file in os.listdir(path_tutorial_docs) if file.endswith(".md")]
+docs = [UnstructuredMarkdownLoader(path_tutorial_docs + "/" + f, mode = "single").load()[0] for f in md_files]
+
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+]
+
+markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+md_header_splitted_docs = [markdown_splitter.split_text(doc.page_content) for doc in docs]
+
+print(md_header_splitted_docs[2])
