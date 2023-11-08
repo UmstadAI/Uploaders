8d1d5498f7240709a94bdaffafd00d69a245ec64
berkingurcan <berkingurcan@yandex.com>
2023-11-08 19:37:17 +0300

    add extract comment as metadata

--- github_project_loader/example_loader/loader.py
+++ github_project_loader/example_loader/loader.py
@@ -3,15 +3,12 @@ import os
 import openai
 import pinecone
 import time
+import re
 
 from uuid import uuid4
 
 from langchain.document_loaders.generic import GenericLoader
 from langchain.document_loaders.parsers import LanguageParser
-from langchain.text_splitter import (
-    RecursiveCharacterTextSplitter,
-    Language,
-)
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv(), override=True) # read local .env file
@@ -21,6 +18,7 @@ pinecone_api_key = os.getenv('PINECONE_API_KEY') or 'YOUR_API_KEY'
 pinecone_env = os.getenv('PINECONE_ENVIRONMENT') or "YOUR_ENV"
 
 base_dir = "./examples/src/examples"
+
 loader = GenericLoader.from_filesystem(
     base_dir,
     glob="**/*",
@@ -33,7 +31,14 @@ docs = loader.load()
 model_name = 'text-embedding-ada-002'
 
 texts = [c.page_content for c in docs]
-metadatas = [c.metadata for c in docs]
+def extract_comments_from_ts_code(ts_code):
+    comment_pattern = r'(\/\/[^\n]*|\/\*[\s\S]*?\*\/)'
+    comments = re.findall(comment_pattern, ts_code)
+    comments_string = ' '.join(comments)
+
+    return comments_string
+
+metadatas = [extract_comments_from_ts_code(c.page_content) for c in docs]
 
 chunks = [texts[i:(i + 1000) if (i+1000) <  len(texts) else len(texts)] for i in range(0, len(texts), 1000)]
 embeds = []
@@ -72,5 +77,15 @@ ids = [str(uuid4()) for _ in range(len(docs))]
 
 vectors = [(ids[i], embeds[i], {
     'text': docs[i].page_content, 
-    'title': docs[i].metadata
-})]
+    'title': metadatas[i]
+}) for i in range(len(docs))]
+
+print(vectors[23])
+
+namespace = "zkappumstad-codebase"
+for i in range(0, len(vectors), 100):
+    batch = vectors[i:i+100]
+    print("Upserting batch:", i)
+    index.upsert(batch, namespace=namespace)
+
+print(index.describe_index_stats())
