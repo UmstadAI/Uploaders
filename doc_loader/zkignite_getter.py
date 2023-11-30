7a4232723e113ba4d5229df070c00442bccd6c46
berkingurcan <berkingurcan@yandex.com>
2023-11-30 16:03:40 +0700

    feat add getting readme files

--- doc_loader/zkignite_getter.py
+++ doc_loader/zkignite_getter.py
@@ -1,7 +1,11 @@
 import os
 import requests
 import re
+import base64
+from github import Github
+
 from dotenv import load_dotenv, find_dotenv
+
 
 _ = load_dotenv(find_dotenv()) # read local .env file
 
@@ -11,26 +15,29 @@ headers = {
     'Authorization': f'token {token}'
 }
 
-def download_readme(url):
-    api_url = re.sub(r'github.com', 'api.github.com/repos', url)
+def download_readme(repo_url):
+    parts = repo_url.strip('/').split('/')
+    owner, repo = parts[-2], parts[-1]
+    print(f"Downloading {owner}/{repo} readme...")
 
-    response = requests.get(api_url, headers=headers)
+    g = Github(token)
+    repo = g.get_repo(f"{owner}/{repo}")
 
-    if response.status_code == 200:
-        readme_content = response.json().get('content', '')
-        return readme_content
-    else:
-        print(f"Failed to download README from {api_url}")
-        return None
+    readme = repo.get_readme()
+    content = base64.b64decode(readme.content)
+
+    return content.decode('utf-8')
+
+    
     
 urls = [
-    "https://github.com/rhvall/MinaDevContainer/blob/Release/README.md",
-    "https://github.com/airgap-it/airgap-coin-lib/blob/master/readme.md",
-    "https://github.com/rloot/ZOK/blob/main/README.md",
-    "https://github.com/orochi-network/zkDatabase/blob/main/README.md",
-    "https://github.com/anomix-zk/anomix-network/blob/main/README.md",
-    "https://github.com/EasyMina/easyMina/blob/main/README.md",
-    "https://github.com/devarend/mina-playground/blob/develop/README.md",
+    "https://github.com/rhvall/MinaDevContainer",
+    "https://github.com/airgap-it/airgap-coin-lib",
+    "https://github.com/rloot/ZOK",
+    "https://github.com/orochi-network/zkDatabase",
+    "https://github.com/anomix-zk/anomix-network",
+    "https://github.com/EasyMina/easyMina",
+    "https://github.com/devarend/mina-playground",
 ]
 
 if not os.path.exists(f"files/zkignite_docs"):
@@ -39,6 +46,6 @@ if not os.path.exists(f"files/zkignite_docs"):
 for url in urls:
     readme = download_readme(url)
     if readme:
-        file_name = url.split('/')[-3] + '_README.md'
-        with open(f"files/{file_name}", 'w') as file:
+        file_name = url.split('/')[-1] + '_README.md'
+        with open(f"files/zkignite_docs/{file_name}", 'w') as file:
             file.write(readme)
