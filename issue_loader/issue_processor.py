baa6ac5c158019805ef23367d6d41e6339825dd8
berkingurcan <berkingurcan@yandex.com>
2023-12-05 09:09:44 +0700

    feat add export data as txt

--- issue_loader/issue_processor.py
+++ issue_loader/issue_processor.py
@@ -1,8 +1,34 @@
 import os
+import ast
+import json
 
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv('output.csv')
 df = df[df['comments'].apply(lambda x: len(x) > 3)]
+
+os.mkdir('issues_json')
+
+for index, row in df.iterrows():
+    writer = row['writer']
+    title = row['title']
+    is_open = row['is_open']
+    issue_body = row['body']
+    comments = row['comments']
+    comments = ast.literal_eval(comments)
+
+    issue = {
+        "title": title,
+        "author": writer,
+        "is_open": is_open,
+        "issue": issue_body,
+        "comments": comments
+    }       
+
+    file_path = f"issues_json/{index}.txt"
+    json_string = json.dumps(issue, indent=4)
+
+    with open(file_path, 'w') as f:
+        f.write(json_string)
 
