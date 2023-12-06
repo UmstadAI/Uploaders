7df983d48f8014dcd2e349252e50bca4cf92881c
berkingurcan <berkingurcan@yandex.com>
2023-12-06 09:08:21 +0700

    read issues

--- issue_loader/loader.py
+++ issue_loader/loader.py
@@ -8,9 +8,7 @@ import json
 
 from uuid import uuid4
 
-from langchain.document_loaders import JSONLoader
-from langchain.document_loaders.generic import GenericLoader
-from langchain.document_loaders.parsers import LanguageParser
+from langchain.document_loaders import DirectoryLoader
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv(), override=True) # read local .env file
@@ -19,13 +17,23 @@ openai.api_key = os.getenv('OPENAI_API_KEY') or 'OPENAI_API_KEY'
 pinecone_api_key = os.getenv('PINECONE_API_KEY') or 'YOUR_API_KEY'
 pinecone_env = os.getenv('PINECONE_ENVIRONMENT') or "YOUR_ENV"
 
-base_dir = "issues_json"
+base_dir = "./issues_json"
 
-loader = GenericLoader.from_filesystem(
-    base_dir,
-    glob="**/*",
-    suffixes=[".json"],
-)
+json_files = glob.glob(os.path.join(base_dir, '**/*.json'), recursive=True)
 
-issues = loader.load()
-print(issues)
+issues = []
+
+for issue_path in json_files:
+    with open(issue_path, 'r') as file:
+        issue = file.read()
+        issue = json.loads(issue)
+
+        try:
+            question = issue['full_question'] + issue['question']
+            answer = issue['answer']
+        except:
+            continue
+
+        issues.append({"question": question, "answer": answer})
+
+print(len(issues))
